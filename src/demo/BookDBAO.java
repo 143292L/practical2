1081c6f75e943fec510d0b46067625462eea1f40
unknown <143292l@CL7G207.sit.nyp.edu.sg>
2015-10-29 17:17:14 +0800

    Initial commit

---
+++ src/demo/BookDBAO.java
@@ -0,0 +1,25 @@
+package demo;
+
+/**
+ * Created by 143292l on 10/29/2015.
+ */
+public class BookDBAO {
+    public BookDetails getBookDetails(String bookId) {
+        // return dummy book for testing now
+        return createDummyBook(bookId);
+    }
+
+    private BookDetails createDummyBook(String bookId) {
+        BookDetails details = new BookDetails();
+        details.setBookId(bookId);
+        details.setDescription("Web components for developers talks about how web components can transfer the way you develop application for the web");
+        details.setFirstName("Java");
+        details.setSurname("Expert");
+        details.setInventory(5);
+        details.setOnSale(false);
+        details.setPrice(5.9f);
+        details.setTitle("Web components for developers");
+        details.setYear(2015);
+        return details;
+    }
+}
